ea91fc918bb4ddc1b76b68ac58e0619f6c903b9b
dhivyansureshkumar <dsur0008@student.monash.edu>
2023-12-03 21:18:40 +1100

    updated homework to implement heap

--- priority_queue.py
+++ priority_queue.py
@@ -1,5 +1,3 @@
-import heapq
-
 class PriorityQueue:
     def __init__(self):
 
@@ -15,19 +13,48 @@ class PriorityQueue:
     
     
     def push(self,value):
-        return heapq.heappush(self.heap, value)
+        self.heap.append(value)
+        self.bubble_up(len(self.heap)-1)
 
 
     def pop(self):
 
         if self.isEmpty():
             return ValueError("heap is empty")
-        return heapq.heapop(self.heap)
-    
+        
+        self.heap[0], self.heap[-1] = self.heap[-1], self.heap[0] #swap top of heap with bottom right
+        popped_value = self.heap.pop()
+        self.bubble_down(0)
+        return popped_value
+
 
     def isEmpty(self):
         return len(self.heap) == 0
     
+
+    def bubble_up(self,index):
+        parent_index = (index - 1) // 2
+        if index > 0 and self.heap[parent_index] > self.heap[index]:
+            self.heap[parent_index], self.heap[index] = self.heap[index], self.heap[parent_index]
+            self.bubble_up(parent_index)
+
+
+
+    def bubble_down(self,index):
+        smallest = index
+        left_child= index * 2 + 1
+        right_child = index * 2 + 2
+
+        if left_child < len(self.heap) and self.heap[left_child] < self.heap[smallest]:
+                smallest = left_child
+
+        if right_child < len(self.heap) and self.heap[right_child] < self.heap[smallest]:
+            smallest = right_child
+
+        if smallest != index:
+            self.heap[index], self.heap[smallest] = self.heap[smallest], self.heap[index]
+            self.heapify_down(smallest)       
+
 
 
 pq = PriorityQueue()
